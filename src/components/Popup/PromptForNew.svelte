9819e6c8eb8864446afa365ea29f6854ccac39d6
daveberrys <84567826+daveberrys@users.noreply.github.com>
2026-08-14 04:17:44 +0800

    add gamebanana and add icons

--- src/components/Popup/PromptForNew.svelte
+++ src/components/Popup/PromptForNew.svelte
@@ -1,28 +1,39 @@
 <script lang="ts">
+    import { openUrl } from "@tauri-apps/plugin-opener";
+
     export let promptForNew: boolean;
     export let modalNew: boolean;
     export let modalDownload: boolean;
 
-    function close(type: string) {
+    const options = [
+      { name: "Local", icon: "desktop_windows", type: "local", primary: true },
+      { name: "Download Engines", icon: "download", type: "download" },
+      { name: "GameBanana", icon: "globe", type: "https://gamebanana.com/games/8694" }
+    ]
+
+    function close(type: string = "") {
         promptForNew = false;
 
         if (type === "local") {
             modalNew = true;
         } else if (type === "download") {
             modalDownload = true;
+        } else if (type.includes("https://")) {
+            openUrl(type);
         }
     }
 </script>
 
 <div class="overlay" class:active={promptForNew} onclick={() => close("")}></div>
-<dialog class:active={promptForNew}>
+<dialog class:active={promptForNew} style="width: 500px;">
   <h5>Adding a new instance for FNF...</h5>
   <div>
       You can either download a new instance...<br/>
       or add in your already-downloaded ones!
   </div>
-  <nav class="right-align no-space">
-    <button onclick={() => close("local")}>Local</button>
-    <button onclick={() => close("download")} class="transparent">Download</button>
+  <nav class="no-space center-align" style="display: flex; flex-wrap: wrap;">
+    {#each options as option}
+        <button onclick={() => close(option.type)} class:transparent={!option.primary}><i>{option.icon}</i>{option.name}</button>
+    {/each}
   </nav>
 </dialog>
